1fd5ae615ddcd61e5672ad66bc8f001d6820b26a
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2022-10-25 22:47:06 +0200

    Added addition and subtraction methods in class Calculator. Use of this metods in main

--- src/main/java/com/kodilla/kalkulaotr/Calculator.java
+++ src/main/java/com/kodilla/kalkulaotr/Calculator.java
@@ -6,9 +6,14 @@ public class Calculator {
         return a+b;
     }
 
+    public int subtract(int a, int b){
+        return a-b;
+    }
 
 
     public static void main(String args[]){
-
+        Calculator calculator = new Calculator();
+        System.out.println("Result of adding numbers 1 and 5 is: " + calculator.add(1, 5));
+        System.out.println("Result of subtracting numbers 5 and 2 is: " + calculator.subtract(5, 2);
     }
 }
