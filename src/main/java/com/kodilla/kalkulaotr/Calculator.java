d5e5873e1ae742a232bb52028902da4a55626ee0
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2022-10-25 22:41:39 +0200

    First commit - added Calculator class

---
+++ src/main/java/com/kodilla/kalkulaotr/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.kalkulaotr;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+
+
+    public static void main(String args[]){
+
+    }
+}
---
+++ src/main/java/com/kodilla/kalkulaotr/KalkulaotrApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kalkulaotr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalkulaotrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalkulaotrApplication.class, args);
+    }
+
+}
