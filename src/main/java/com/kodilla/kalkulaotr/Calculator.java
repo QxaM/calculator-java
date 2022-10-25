7257bac2f57298046b5a937468bc4057bcd2cf57
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2022-10-25 22:48:39 +0200

    Tested Calculator class in main

--- src/main/java/com/kodilla/kalkulaotr/Calculator.java
+++ src/main/java/com/kodilla/kalkulaotr/Calculator.java
@@ -14,6 +14,6 @@ public class Calculator {
     public static void main(String args[]){
         Calculator calculator = new Calculator();
         System.out.println("Result of adding numbers 1 and 5 is: " + calculator.add(1, 5));
-        System.out.println("Result of subtracting numbers 5 and 2 is: " + calculator.subtract(5, 2);
+        System.out.println("Result of subtracting numbers 5 and 2 is: " + calculator.subtract(5, 2));
     }
 }
